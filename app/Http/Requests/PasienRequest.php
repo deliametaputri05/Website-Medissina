2466216c4d9e66213a451749093f269a730d1c38
deliametaputri05 <deliametaputri05@gmail.com>
2023-01-02 10:52:15 +0700

    create request

---
+++ app/Http/Requests/PasienRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PasienRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        return [
+
+            'id_poli' => 'required|exists:poli,id',
+            'id_dokter' => 'required|exists:dokter,id',
+            'nik' => 'required|integer',
+            'nama' => 'required|string',
+            'tempatLahir' => 'required|string',
+            'tglLahir' => 'required|date',
+            'usia' => 'required|integer',
+            'jk' => 'required|string',
+            'agama' => 'required|string',
+            'status' => 'required|string',
+            'alamat' => 'required|string',
+            'noHp' => 'required|string',
+            'caraBayar' => 'required|string',
+            'ktp' => 'required|image',
+            'tglBerobat' => 'required|date',
+            'keluhan' => 'required|string',
+            'ceklis' => 'required|string'
+
+        ];
+    }
+}
---
+++ app/Http/Requests/TestimoniRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TestmoniRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        return [
+
+            'nama' => 'required|string',
+            'pekerjaan' => 'required|string',
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'gambar' => 'image',
+            'pesan' => 'required|string',
+            'tipe' => 'required|string'
+
+        ];
+    }
+}
